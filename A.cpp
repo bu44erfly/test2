7ed5176d91abeee3e5a9fbf7a4c046ce82d9ec3d
bu44erfly <y24662319@gmail.com>
2024-02-21 01:50:08 +0800

    FIrst edition

--- A.cpp
+++ A.cpp
@@ -1,50 +1,11 @@
-#include <iostream>
-#include <cstring> 
-using namespace std;
- const int N =3e6+4;
- int tot,a[N],ch[N][26];
- int val[N],End[N];
-
- void insert(int len){
- 	int u=0;
- 	
- 	for(int i=1;i<=len;i++){
- 		int c=a[i];
- 		
- 		if(ch[u][c]==0) ch[u][c]=++tot;
- 		u=ch[u][c];
- 		val[u]++;
- 	}
- 	End[u]++;
- }
- void find(int len){
- 	int u=0,flag=1;
-	int ans=0;
- 	
- 	for(int i=1;i<=len;i++){
- 		int c=a[i];
- 		if(ch[u][c]==0){ flag=0; break;}
- 		
- 		u=ch[u][c];
- 		ans+=End[u];
- 	}
- 	if(flag) ans=ans+val[u]-End[u];
- 	cout<<ans<<endl;
- }
- signed main(){
- 	int n,q,tes;
- 	cin>>n>>q;
- 	for(int i=1;i<=n;i++){
- 		cin>>tes;
- 		for(int j=1;j<=tes;j++) cin>>a[j];
- 		insert(tes);
- 	}
- 	for(int i=1;i<=q;i++){
- 		cin>>tes;
- 		for(int j=1;j<=tes;j++) cin>>a[j];
- 		find(tes);
- 	}
- }
- 
- 
-
+function<vector<int>(int,int) > dfs = [&](int x, int fa) -> vector<int> {
+            vector<int> a = {cost[x]};
+            for (int y: g[x]) {
+                if (y != fa) {
+                    auto res = dfs(y, x);
+                    for(auto k:res) a.push_back(k); 
+                }
+            }
+            //
+            return a;
+        };
